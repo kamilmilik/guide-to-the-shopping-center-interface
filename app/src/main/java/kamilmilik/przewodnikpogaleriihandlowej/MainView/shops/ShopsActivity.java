3c3d9f205741660a66416eef420b05ae7c7c9a61
Kamil Milik <kamilmilik@gmail.com>
2018-01-21 21:22:28 +0100

    Add filter to shops

--- app/src/main/java/kamilmilik/przewodnikpogaleriihandlowej/MainView/shops/ShopsActivity.java
+++ app/src/main/java/kamilmilik/przewodnikpogaleriihandlowej/MainView/shops/ShopsActivity.java
@@ -6,18 +6,29 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import kamilmilik.przewodnikpogaleriihandlowej.MainView.login.LoginActivity;
 import kamilmilik.przewodnikpogaleriihandlowej.R;
 
 public class ShopsActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private  String[] labelsArray1,labelsArray2;
+    private int[] imagesArray;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +44,9 @@ public class ShopsActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        String[] labelsArray1 = {"4f","five5asec", "ALDO","Apart","Benneton","Bershka","BIG STAR","C&A","Dr Max","Online"};
-        String[] labelsArray2 = {"0, #121","1, #1234","-1, #123425","1, #1765","1, #354","1, #12341","1, #1221","1, #1261","1, #1218","1, #1921"};
-        int[] imagesArray = {R.drawable.four4f, R.drawable.five5asec,R.drawable.aldo,R.drawable.apart,R.drawable.benneton,R.drawable.bershka,R.drawable.bigstar,R.drawable.ca,R.drawable.a1635,R.drawable.online};
+        labelsArray1 = new String[]{"4f", "5asec", "ALDO", "Apart", "Benneton", "Bershka", "BIG STAR", "C&A", "Dr Max", "Online"};
+        labelsArray2 = new String[]{"0, #121", "1, #1234", "-1, #123425", "1, #1765", "1, #354", "1, #12341", "1, #1221", "1, #1261", "1, #1218", "1, #1921"};
+        imagesArray = new int[]{R.drawable.four4f, R.drawable.five5asec, R.drawable.aldo, R.drawable.apart, R.drawable.benneton, R.drawable.bershka, R.drawable.bigstar, R.drawable.ca, R.drawable.a1635, R.drawable.online};
         mAdapter = new MyAdapter(labelsArray1,labelsArray2,imagesArray);
         mRecyclerView.setAdapter(mAdapter);
 
@@ -46,7 +57,101 @@ public class ShopsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
+    //menu
+    private Menu optionsMenu;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_check_box, menu);
 
+        optionsMenu = menu;
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.checkable_item_1:
+                if(item.isChecked()){
+                    makeDefaultStateOfRecyclerViewItems();
+                    item.setChecked(false);
+                }
+                else {
+                    labelsArray1 = new String[]{"4f", "Benneton","Bershka", "BIG STAR", "C&A"};
+                    labelsArray2 = new String[]{"0, #121","1, #354", "1, #354", "1, #12341", "1, #1221", "1, #1261"};
+                    imagesArray = new int[]{R.drawable.four4f,R.drawable.benneton, R.drawable.bershka, R.drawable.bigstar, R.drawable.ca};
+                    mAdapter = new MyAdapter(labelsArray1,labelsArray2,imagesArray);
+                    mRecyclerView.setAdapter(mAdapter);
+                    item.setChecked(true);
+                }
+                optionsMenu.findItem(R.id.checkable_item_2).setChecked(false);
+                optionsMenu.findItem(R.id.checkable_item_3).setChecked(false);
+                optionsMenu.findItem(R.id.checkable_item_4).setChecked(false);
+                return true;
+            case R.id.checkable_item_2:
+                if(item.isChecked()){
+                    makeDefaultStateOfRecyclerViewItems();
+                    item.setChecked(false);
+                }
+                else {
+                    labelsArray1 = new String[]{"5asec"};
+                    labelsArray2 = new String[]{"1, #1234"};
+                    imagesArray = new int[]{R.drawable.five5asec};
+                    mAdapter = new MyAdapter(labelsArray1,labelsArray2,imagesArray);
+                    mRecyclerView.setAdapter(mAdapter);
+                    item.setChecked(true);
+                }
+                optionsMenu.findItem(R.id.checkable_item_1).setChecked(false);
+                optionsMenu.findItem(R.id.checkable_item_3).setChecked(false);
+                optionsMenu.findItem(R.id.checkable_item_4).setChecked(false);
+                return true;
+            case R.id.checkable_item_3:
+                if(item.isChecked()){
+                    makeDefaultStateOfRecyclerViewItems();
+                    item.setChecked(false);
+                }
+                else {
+                    labelsArray1 = new String[]{"Aldo"};
+                    labelsArray2 = new String[]{"-1, #123425"};
+                    imagesArray = new int[]{R.drawable.aldo};
+                    mAdapter = new MyAdapter(labelsArray1,labelsArray2,imagesArray);
+                    mRecyclerView.setAdapter(mAdapter);
+                    item.setChecked(true);
+                }
+                optionsMenu.findItem(R.id.checkable_item_1).setChecked(false);
+                optionsMenu.findItem(R.id.checkable_item_2).setChecked(false);
+                optionsMenu.findItem(R.id.checkable_item_4).setChecked(false);
+                return true;
+            case R.id.checkable_item_4:
+                if(item.isChecked()){
+                    makeDefaultStateOfRecyclerViewItems();
+                    item.setChecked(false);
+                }
+                else {
+                    labelsArray1 = new String[]{"Dr Max"};
+                    labelsArray2 = new String[]{"1, #1921"};
+                    imagesArray = new int[]{R.drawable.a1635};
+                    mAdapter = new MyAdapter(labelsArray1,labelsArray2,imagesArray);
+                    mRecyclerView.setAdapter(mAdapter);
+                    item.setChecked(true);
+                }
+                optionsMenu.findItem(R.id.checkable_item_1).setChecked(false);
+                optionsMenu.findItem(R.id.checkable_item_2).setChecked(false);
+                optionsMenu.findItem(R.id.checkable_item_3).setChecked(false);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+    public void makeDefaultStateOfRecyclerViewItems(){
+        labelsArray1 = new String[]{"4f", "5asec", "ALDO", "Apart", "Benneton", "Bershka", "BIG STAR", "C&A", "Dr Max", "Online"};
+        labelsArray2 = new String[]{"0, #121", "1, #1234", "-1, #123425", "1, #1765", "1, #354", "1, #12341", "1, #1221", "1, #1261", "1, #1218", "1, #1921"};
+        imagesArray = new int[]{R.drawable.four4f, R.drawable.five5asec, R.drawable.aldo, R.drawable.apart, R.drawable.benneton, R.drawable.bershka, R.drawable.bigstar, R.drawable.ca, R.drawable.a1635, R.drawable.online};
+        mAdapter = new MyAdapter(labelsArray1,labelsArray2,imagesArray);
+        mRecyclerView.setAdapter(mAdapter);
+    }
 
     public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         private String[] labelTextArray1;
